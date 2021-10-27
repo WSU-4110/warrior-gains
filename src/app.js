95a19e694240067dc034effd50910eaf648b6c43
Opy Ali <gn8258@wayne.edu>
2021-10-27 18:39:58 -0400

    designed feed page, added navigation bar, and put background color

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 const userSchema = {
