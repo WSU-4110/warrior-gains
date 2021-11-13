f65ab3a8e0b250adbbe11f3be0f75d36d56d0de7
Omar Faruk <gw2625@wayne.edu>
2021-11-12 23:28:53 -0500

    Verify that user input meets requirements before allowing user to complete whole registration process. Added error messages to display when reqs are invalid. Added password confirmation, 18+ age req, and wayne.edu email.

--- src/app.js
+++ src/app.js
@@ -84,7 +84,6 @@ app.post("/register", function (req, res) {
     User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
-            res.redirect("/register");
         } else {
             passport.authenticate("local")(req, res, function () {
                 res.redirect("/feed")
