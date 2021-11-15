e16e853a385ba85593f1eb8f9e3aab5924eaaf47
Omar Faruk <gw2625@wayne.edu>
2021-11-14 20:45:11 -0500

    Add two models to appropriately store user information on DB. Configured deserialze user session. Also made sure logout on feed actually logs user out

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
@@ -24,13 +24,18 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Create database using MongoDB to store user's name, email, password, and date of birth.
+// Make database connection to local MongoDB
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 
+// User credentials model
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
+});
+
+// User info model
+const nameSchema = new mongoose.Schema({
     fname: String,
     lname: String,
     dob: Date
@@ -40,6 +45,8 @@ const userSchema = new mongoose.Schema({
 userSchema.plugin(passportLocalMongoose)
 
 const User = new mongoose.model("User", userSchema)
+const Name = new mongoose.model("Name", nameSchema)
+
 
 // Simplified Passport/Passport-Local Configuration
 passport.use(User.createStrategy());
@@ -48,17 +55,25 @@ passport.serializeUser(function (user, done) {
     done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-    done(null, user);
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
 });
+
+// passport.deserializeUser(function (user, done) {
+//     done(null, user);
+// });
 
 // Routes for users to traverse the webpage
 app.get("/", function (req, res) {
     res.render("home")
+
 })
 
 app.get("/login", function (req, res) {
     res.render("login")
+
 })
 
 app.get("/register", function (req, res) {
@@ -72,6 +87,13 @@ app.get("/profile", function (req, res) {
 app.get("/feed", function (req, res) {
     if (req.isAuthenticated()) {
         res.render("feed");
+        // User.find({}, function (firstname) {
+        //     res.render("feed");
+        //     firstName: firstname
+        // })
+
+        // res.sendFile(__dirname + "/views/feed.html")
+
     }
     else {
         res.redirect("/login");
@@ -86,6 +108,15 @@ app.get("/logout", function (req, res) {
 // If the user is able to register successfully, they may have access to feed page
 app.post("/register", function (req, res) {
 
+    const userInfo = new Name({
+        email: req.body.username,
+        fname: req.body.fname,
+        lname: req.body.lname,
+        dob: req.body.dob,
+    })
+
+    userInfo.save();
+
     User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
@@ -98,20 +129,22 @@ app.post("/register", function (req, res) {
 
 })
 
-// If the user is able to login with email and password, they will be redirected to feed page
+//If the user is able to login with email and password, they will be redirected to feed page
 
 // app.post("/login", function (req, res) {
 //     const user = new User({
 //         username: req.body.username,
-//         password: req.body.password
+//         password: req.body.password,
+
 //     });
 
 //     req.login(user, function (err) {
 //         if (err) {
 //             console.log(err);
 //         } else {
-//             passport.authenticate("local")(req, res, function () {
-//                 res.redirect("/feed");
+//             passport.authenticate("local", {
+//                 successRedirect: '/feed',
+//                 failureRedirect: '/login'
 //             });
 //         }
 //     });
